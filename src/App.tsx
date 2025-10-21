b1a761d8a39a6c2b0d84845bff63c119710c2c0e
wpinocho <julian.ruiz.loza@gmail.com>
2025-10-21 15:48:25 -0600

    AI Update: 1 files updated by Lovivo

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,18 @@ import { CartProvider } from "@/contexts/CartContext";
 import { CartUIProvider } from "@/components/CartProvider";
 import { SettingsProvider } from "@/contexts/SettingsContext";
 import { PixelProvider } from "@/contexts/PixelContext";
+import { AuthProvider } from "@/contexts/AuthContext";
 import Index from "./pages/Index";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import ThankYou from "./pages/ThankYou";
+import Blog from "./pages/Blog";
+import BlogPost from "./pages/BlogPost";
+import MyOrders from "./pages/MyOrders";
+import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
@@ -17,21 +26,31 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <SettingsProvider>
       <PixelProvider>
-        <CartProvider>
-          <CartUIProvider>
-            <TooltipProvider>
-              <Toaster />
-              <Sonner />
-              <BrowserRouter>
-                <Routes>
-                  <Route path="/" element={<Index />} />
-                  <Route path="/about" element={<About />} />
-                  <Route path="/contact" element={<Contact />} />
-                </Routes>
-              </BrowserRouter>
-            </TooltipProvider>
-          </CartUIProvider>
-        </CartProvider>
+        <AuthProvider>
+          <CartProvider>
+            <CartUIProvider>
+              <TooltipProvider>
+                <Toaster />
+                <Sonner />
+                <BrowserRouter>
+                  <Routes>
+                    <Route path="/" element={<Index />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/products/:slug" element={<Product />} />
+                    <Route path="/cart" element={<Cart />} />
+                    <Route path="/checkout" element={<Checkout />} />
+                    <Route path="/thank-you/:orderId" element={<ThankYou />} />
+                    <Route path="/blog" element={<Blog />} />
+                    <Route path="/blog/:slug" element={<BlogPost />} />
+                    <Route path="/my-orders" element={<MyOrders />} />
+                    <Route path="*" element={<NotFound />} />
+                  </Routes>
+                </BrowserRouter>
+              </TooltipProvider>
+            </CartUIProvider>
+          </CartProvider>
+        </AuthProvider>
       </PixelProvider>
     </SettingsProvider>
   </QueryClientProvider>
